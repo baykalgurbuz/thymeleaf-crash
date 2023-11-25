075cb0c93b07bb4af900d5059b49ee9653b3217c
baykalgurbuz <ebaykalgurbuz@gmail.com>
2023-11-25 13:30:55 +0300

    Data transfer between forms

--- src/main/java/com/coolapp/thymeleafapp/controller/HelloWorldController.java
+++ src/main/java/com/coolapp/thymeleafapp/controller/HelloWorldController.java
@@ -3,11 +3,14 @@ package com.coolapp.thymeleafapp.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm()
     {
         return "helloworld-form";
@@ -23,6 +26,14 @@ public class HelloWorldController {
         String theName=request.getParameter("studentName");
         theName=theName.toUpperCase();
         String result ="Yo!"+theName;
+        model.addAttribute("message",result);
+        return "helloworld";
+    }
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model)
+    {
+        theName=theName.toUpperCase();
+        String result ="Process V3"+theName;
         model.addAttribute("message",result);
         return "helloworld";
     }
